3883ce2d2c8391759aa3aaba8be3899008c62808
Djordje Knezevic <starwarrior@mycity.rs>
2021-11-22 19:56:32 +0100

    Add dynamic pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router';
+
 // domain.com/news/something-important
 
 function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
     return <h1>The Detil Page</h1>
 };
 
